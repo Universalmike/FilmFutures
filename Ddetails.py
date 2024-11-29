1c2e24227b33aee5a944ff8dc281e7a4e96a6901
Universalmike <150071045+Universalmike@users.noreply.github.com>
2024-11-30 00:51:49 +0100

    Updated movie details

--- Ddetails.py
+++ Ddetails.py
@@ -1,47 +1,70 @@
 def movie_details(part):
     if part == 1:
-        return "1. Joker: Folie à Deux Movie (October 4th). R. 2h18m. Crime, Drama, Thriller."
+        return "A Nonsense Christmas with Sabrina Carpenter"
     elif part == 2:
-        return "2. Hellboy: The Crooked Man (September 27). R. 1h39m. Action, Horror."
+        return "Mufasa: The Lion King"
     elif part == 3:
-        return "3. Farmer's Bride (September 27). 1h29m. Drama."
+        return "Nofseratu"
     elif part == 4:
-        return "4. A Night in 2005 (October 11). 1h40m. Drama."
+        return "The Lord of the Rings: The War of the Rohirrim"
     elif part == 5:
-        return "5. Venom: The Last Dance (October 25). Action, Adventure, Sci-Fi."
+        return "Seven Doors"
     else:
         return "This is everything"
 
 def movie_info(parts):
     if parts == 1:
         return (
-            "Stars: Joaquin Pheonix, Lady Gaga \n\n"
-            "“Joker: Folie À Deux” finds Arthur Fleck institutionalized at Arkham awaiting trial for his crimes as Joker. While struggling with his dual identity, Arthur not only stumbles upon true love, but also finds the music that's always been inside him."
-            "(credit: IMDB)"
+            "Stars: Sabrina Carpenter \n\n"
+            "Released Date: December 6th \n\n"
+            "MOVIE \n\n"
+            "RATED: PG \n\n"
+            "RUNTIME: 2h18m \n\n"
+            "Genre: Music, Holiday \n\n"
+            "It’s a nonsense holiday and we’re so here for it. In this special with musical guests, pop icon Sabrina Carpenter will perform songs from her holiday EP fruitcake and other iconic chart-topping holiday covers. This special will also feature show-stopping music performances, comedic guests, unexpected duets, plus many more surprises and fun cameos. \n\n"
+            "(credit: Movie Insider)"
         )
     elif parts == 2:
         return (
-            "Stars: Jack Kesy, Jeffery White, Leah McNamara\n\n"
-            "Hellboy and a rookie B.P.R.D. agent in the 1950s are sent to the Appalachians, where they discover a remote community dominated by witches and led by the sinister local demon, the Crooked Man."
+            "Stars: Seth Rogen, Thandiwe Newton, Mads Mikkelsen\n\n"
+            "Released Date: December 20th \n\n"
+            "MOVIE \n\n"
+            "RATED: PG \n\n"
+            "RUNTIME: 1h39m \n\n"
+            "Genres: Animation, Adventure \n\n"
+            "Told in flashbacks—Rafiki, Timon and Pumbaa tell the story of Mufasa to a young lion cub— Mufasa reveals the rise of one of the greatest kings of the Pride Lands. \n\n"
             "(credit: MovieInsider)"
         )
     elif parts == 3:
         return (
-            "Stars:Mercy Aigbe, Tobi Bakare,, Femi Branch\n\n"
-            "A wealthy lonely farmer with no surviving family saves for his late brother's widow Morenike and her son Femi, who decides to marry a young bride Funmi from the neighbouring village. "
-            "Funmi who is supposed to sooth Odun's aging nerves and bring him joy in his old age sinks into grief and despair, resenting her husband through this union arranged by her mother Tundun, she had no say in "
-            "(credit: Silverbird)"
+            "Stars: Emma Corrin, Aaron Taylor-Johnson, Bill Skarsgård \n\n"
+            "Release Date: December 25th"
+            "MOVIE \n\n"
+            "RATED: R. \n\n"
+            "RUNTIME: 2h13m \n\n"
+            "Genres: Horror, Mystery \n\n"
+            "A gothic tale of obsession between a haunted young woman and the terrifying vampire infatuated with her, causing untold horror in its wake. \n\n"
+            "(credit: IMDB)"
         )
     elif parts == 4:
         return (
-            "Stars: Harriet Akintola, Bimbo Akintola, Chris Akwarandu\n\n"
-            "Set in 2005, the story unravels one fateful night when everything changes in the blink of an eye. It is a gripping drama that explores themes of love, friendship, and family."
-            "(credit: BellaNaija)"
+            "Stars: Brian Cox, Christopher Lee, Miranda Otto\n\n"
+            "RELEASE DATE: December  13"
+            "MOVIE \n\n"
+            "RATED: PG-13 \n\n"
+            "RUNTIME: 2h14m \n\n"
+            "Genres: Dark Fantasy, Animation \n\n"
+            "A sudden attack by Wulf, a clever and ruthless Dunlending lord seeking vengeance for the death of his father, forces Helm Hammerhand, the King of Rohan, and his people to make a daring last stand in the ancient stronghold of the Hornburg. \n\n"
+            "(credit: IMDB)"
         )
     elif parts == 5:
         return (
-            "Stars: Tom Hardy, Juno Temple\n\n"
-            "Eddie and Venom are on the run. Hunted by both of their worlds and with the net closing in, the duo are forced into a devastating decision that will bring the curtains down on Venom and Eddie's last dance"
+            "Stars: Femi Adebayo, Chioma Chukwuka Akpotha\n\n"
+            "RELEASE DATE: December 13th"
+            "MOVIE \n\n"
+            "RATED: PG"
+            "GENRES: Drama, Hisatory"
+            "Seven Doors is set in two eras, the 18th century and the 19th century, it is about a Yoruba king mar­ried to an Igbo woman, with a Hausa man coming to invest in their kingdom. \n\n"
             "(credit: IMDB)"
         )
     else:
@@ -81,3 +104,14 @@ def blog_details(blogs):
             "- A powerhouse cast that delivers intense and memorable performances."
         )
 
+
+def movie_cover(parted):
+    # Replace with actual URLs for movie covers
+    cover_urls = {
+        1: "FilmFutures/christmas.jpg",
+        2: "FilmFutures/mufasa.jpg",
+        3: "FilmFutures/nosferatu.jpg",
+        4: "FilmFutures/LOTR2.jpg",
+        5: "FilmFutures/sevendoors.jpg",
+    }
+    return cover_urls.get(parted, "C:/Users/BABA/DEFAUL.png")
